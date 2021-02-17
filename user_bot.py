1a47b9a5b54744aa65f708fec57d30fbd2942164
Xuan Canh <xuancanhit99@gmail.com>
2021-02-18 01:36:44 +0300

    update level 2

--- user_bot.py
+++ user_bot.py
@@ -9,26 +9,32 @@ def script(check, x, y):
         if check('player', x+2, y) != check('wall', x, y):
             return 'right'
         return 'down'
-    elif check('level') == 2:
-        print(str(x) + " " + str(y) + "\n")
 
+    elif check('level') == 2:
+        # Debug
+        # print(str(x) + " " + str(y) + "\n")
+
+        # Re phai ngay buoc dau tien(1 buoc cach tuong 1 o)
         if (check('player', x+2, y) != check('wall', x, y)) and (check('player', x, y + 1) == check('wall', x, y)):
             return 'right'
-        if check('player', x, y-1) == check('wall', x + 2, y):
+        # Dieu kien Re phai:
+        # Neu buoc tiep theo ben phai khong phai tuong(cach tuong 1 o trong)
+        # Hoac buoc tiep theo khong co gold => tiep tuc di phai(Re phai o doan cuoi)
+        if (check('player', x, y-1) == check('wall', x + 2, y)) or (check("gold", x + 1, y) != 0):
             return 'right'
-        if (check('player', x, y-1) != check('wall', x, y)) and check('player', x - 2, y + 1) == check('wall', x, y):
+        # Len neu buoc tiep theo len tren khong co tuong(sat tuong) va so lan len cach deu 8 o bat dau tu vi tri 1
+        if check('player', x, y-1) != check('wall', x, y) and ((x - 1) % 8 == 0):
             return 'up'
-        elif check('player', x, y + 1) != check('wall', x, y) and check('player', x + 2, y - 1) == check('wall', x, y):
+        # Xuong neu buoc tiep theo di xuong khong co tuong(sat tuong) va so lan xuong cach deu 8 o bat dau tu vi tri 5
+        if check('player', x, y + 1) != check('wall', x, y) and ((y + 3) % 8 == 0):
             return 'down'
-
-
-
-
-
-
-
-
-
+        # Xuong de khong bi dung yen(Tac dong nhan vat)
+        return 'down'
+    elif check('level') == 3:
+        # Debug
+        print(str(x) + " " + str(y) + "\n")
+        if (check('player', x+1, y) != check('wall', x, y)):
+            return 'right'
 
 
 
